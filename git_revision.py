907c8becf8d40fcc7eb805336f4fc9765e80edc2
Kluvens <95273765+Kluvens@users.noreply.github.com>
2022-04-23 09:20:17 +1000

    Create git_revision.py
    updates

---
+++ git_revision.py
@@ -0,0 +1,16 @@
+to effectively work on large projects, we need:
+  - version control: tracks changes to our code
+  - concurrent programming: multiple people to work on the same files at the same time
+Git is a version control tool that enables to people to work concurrently on the same codebase.
+Git is a distributed version control software.
+
+Git - Single Machine
+Stage 1. version control on a single machine
+Getting setup: SSH keys, git clone
+Status of work: git status, git log
+Doing work: git add, git diff, git commit, git push
+
+Git - Multiple of your machines
+Stage 2. version control across multiple of your machines
+Multiple machines: git pull, merge conflicts
+
