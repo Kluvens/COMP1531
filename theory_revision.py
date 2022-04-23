267e13aa453d6d0fa874494ac10aae6872a12413
Kluvens <95273765+Kluvens@users.noreply.github.com>
2022-04-23 14:54:22 +1000

    Create theory_revision.py

---
+++ theory_revision.py
@@ -0,0 +1,8 @@
+historically use waterfall method: requirements -> design -> implementation -> testing -> deployment&maintenance
+wasterfall has never been proposed as a viable software methodology.
+Agile is an iterative approach to project management and software development that helps teams deliver value to their customers faster and with fewer headaches.
+standups: frequent short progress update meetings
+taskboards: use them to store and track your progress on user stories
+sprints: a fixed of time where you set a number of tasks to be completed in the team. After that period is up, your review progress, and set tracks for the next sprint.
+meeting minutes: just like the notes in a regular team meeting (including attendees, agenda, discussion points, actions)
+pair programming: two programmers, one computer, one keyboard. Take turns to write codes but discuss it as they go
