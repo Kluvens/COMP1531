88b67bdcb9a9c20097658782ab442e9ab9e1d54d
Kluvens <95273765+Kluvens@users.noreply.github.com>
2022-04-23 18:15:48 +1000

    Update code_revision.py

--- code_revision.py
+++ code_revision.py
@@ -29,3 +29,49 @@ finally:
 def test_zero_division():
   with pytest.raises(ZeroDivisionError):
       1 / 0
+
+Flask example:
+from json import dumps
+from flask import Flask, request
+APP = Flask(__name__)
+@APP.route("/search/v1", methods=['GET'])
+def search_request():
+    token = request.args.get('token')
+    query_str = request.args.get('query_str')
+    return dumps(search_v1(token, query_str))
+@APP.route("/message/pin/v1", methods=['POST'])
+def message_pin_http():
+    data = request.get_json()
+    token = data['token']
+    message_id = data['message_id']
+    return dumps(message_pin_v1(token, message_id))
+
+API testing:
+# setup is a fixture and fixture is a decorator
+def test_channel_create_token_error(setup):
+    response = requests.post(
+        f"{url}/channels/create/v2", 
+        json={
+            "token": "Invalid token",
+            "name": "this_channel",
+            "is_public": True}
+        )
+
+    assert response.status_code == AccessError.code
+def test_channel_details_working_single_member(setup):
+    user_dict = setup[0]
+    channel_dict = setup[2]
+
+    token = user_dict['token']
+    channel_id = channel_dict['channel_id']
+
+
+    response = requests.get(
+        f'{url}/channel/details/v2', 
+        params={'token': token,
+        'channel_id': int(channel_id)})
+    
+    assert response.status_code == OKAY
+    first_channel_details = response.json()
+
+    assert first_channel_details['name'] == "this_channel"
