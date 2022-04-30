956c4b7ac2be4d2c23028a1020f9696fd7d42069
Kluvens <95273765+Kluvens@users.noreply.github.com>
2022-04-30 18:55:01 +1000

    ex

--- dict_ex.py
+++ dict_ex.py
@@ -1,1 +1,9 @@
+# extract unique values dictionary values and finally return as a list
+def extract_values(this_dict):
+    result = []
+    for key in this_dict.keys():
+        for value in this_dict[key]:
+            if value not in result:
+                result.append(value)
 
+    return sorted(result)
