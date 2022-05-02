972966a152db44f4389d24ebd4b8d8f9c2dd62a1
Kluvens <95273765+Kluvens@users.noreply.github.com>
2022-05-02 13:56:51 +1000

    updates

--- dict_ex.py
+++ dict_ex.py
@@ -7,3 +7,37 @@ def extract_values(this_dict):
                 result.append(value)
 
     return sorted(result)
+
+# find the sum of all items in a dictionary
+def sum_values(this_dict):
+    sum = 0
+    for key in this_dict.keys():
+        sum += this_dict[key]
+
+    return sum
+
+# remove a key from a dictionary
+def remove_key(this_dict, this_key):
+    this_dict.pop(this_key)
+    return this_dict
+
+# sort a list of dictionaries by a specific value('age')
+def sort_dict_by_values(this_list):
+    return sorted(this_list, key= lambda i: i['age'])
+
+# merge two dictionaries
+def merge_dicts(dict_one, dict_two):
+    dict_one.update(dict_two)
+    return dict_one
+
+# Given a dictionary, perform append of keys followed by values in list.
+def keys_plus_values(this_dict):
+    return list(this_dict.keys()) + list(this_dict.values())
+
+# sort dictionary key and values list
+def sort_with_key_values(this_dict):
+    result = {}
+    for key in sorted(this_dict):
+        result[key] = sorted(this_dict[key])
+
+    return result
--- dict_ex_test.py
+++ dict_ex_test.py
@@ -1,1 +1,39 @@
+from dict_ex import extract_values
+from dict_ex import sum_values
+from dict_ex import remove_key
+from dict_ex import sort_dict_by_values
+from dict_ex import merge_dicts
+from dict_ex import keys_plus_values
+from dict_ex import sort_with_key_values
 
+def test_extract_values():
+    test_dict = {'gfg' : [5, 6, 7, 8],
+             'is' : [10, 11, 7, 5],
+             'best' : [6, 12, 10, 8],
+             'for' : [1, 2, 5]}
+    assert extract_values(test_dict) == [1, 2, 5, 6, 7, 8, 10, 11, 12]
+
+def test_sum_values():
+    test_dict = {'a': 100, 'b': 200, 'c': 300}
+    assert sum_values(test_dict) == 600
+
+def test_remove_key():
+    test_dict = {'a': 100, 'b': 200, 'c': 300}
+    assert remove_key(test_dict, 'a') == {'b': 200, 'c': 300}
+
+def test_sort_dict_by_values():
+    test_dict = [{ "name" : "Nandini", "age" : 20},
+                { "name" : "Manjeet", "age" : 20 },
+                { "name" : "Nikhil" , "age" : 19 }]
+    assert sort_dict_by_values(test_dict) == [{'age': 19, 'name': 'Nikhil'}, {'age': 20, 'name': 'Nandini'}, {'age': 20, 'name': 'Manjeet'}]
+
+def test_merge_dicts():
+    dict1 = {'a': 10, 'b': 8}
+    dict2 = {'d': 6, 'c': 4}
+    assert merge_dicts(dict1, dict2) == {'a': 10, 'b': 8, 'd': 6, 'c': 4}
+
+def test_keys_plus_values():
+    assert keys_plus_values({"Gfg" : 1, "is" :  3, "Best" : 2}) == ['Gfg', 'is', 'Best', 1, 3, 2]
+
+def test_sort_keys_values():
+    assert sort_with_key_values({"gfg": [7, 6, 3], "is": [2, 10, 3], "best": [19, 4]}) == {"best": [4, 19], "gfg": [3, 6, 7], "is": [2, 3, 10]}
